0da3dc7ac8dfdba93c3e8281b258091317130011
dream0ver <krishnachaithanya826@gmail.com>
2023-03-11 18:36:03 +0530

    initial commit

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,4 +1,12 @@
 import { para } from "./data";
 export default function Body() {
-  return <div className="body-container">{para}</div>;
+  return (
+    <div className="body-container">
+      <iframe
+        src="https://dazzling-boba-33e0d2.netlify.app/"
+        height={"99%"}
+        width={"100%"}
+      ></iframe>
+    </div>
+  );
 }
